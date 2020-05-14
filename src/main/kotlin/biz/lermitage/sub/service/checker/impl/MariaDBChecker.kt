c2b930a04c25bd2fc27945f83b9263e7476debd0
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-05-14 02:49:44 +0200

    improve version detection for MariaDB
    add an indication when version is not a Stable release: RC, etc.

--- src/main/kotlin/biz/lermitage/sub/service/checker/impl/MariaDBChecker.kt
+++ src/main/kotlin/biz/lermitage/sub/service/checker/impl/MariaDBChecker.kt
@@ -17,7 +17,14 @@ abstract class MariaDBChecker(private val versionLeftPart: String) : Checker {
             .followRedirects(Globals.SCRAPPER_FOLLOW_REDIRECTS)
             .userAgent(Globals.SCRAPPER_USER_AGENT)
             .execute().parse().body()
-        val version = body.getElementsByAttributeValueStarting("href", "/mariadb/${versionLeftPart}.")[0].text()
+        val versionLnkInHistoryTable = body.getElementsByAttributeValueStarting("href", "/mariadb/${versionLeftPart}.")[0]
+        val versionNumber = versionLnkInHistoryTable.text()
+        val versionType = versionLnkInHistoryTable.parent().parent().getElementsByTag("td")[2].text()
+        val version = if (versionType.toLowerCase().contains("stable")) {
+            versionNumber
+        } else {
+            "$versionNumber $versionType"
+        }
 
         return SoftwareUpdate(
             "MariaDB $versionLeftPart",
