3a277cc1276b3f72e2c1d297a0f4c4d85b13cd9c
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-05-17 18:07:58 +0200

    force push is useless

--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
 java -jar build/libs/software-updates-bot-0.0.1-SNAPSHOT.jar
 git add report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml
 git commit report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml -m"autocommit by bot"
-git push --force
+git push
