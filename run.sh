66c368793405d45b11bc51bcdec91317bd232e39
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-05-18 19:17:21 +0200

    log git output

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 java -jar build/libs/software-updates-bot-0.0.1-SNAPSHOT.jar
-git add report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml
-git commit report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml -m"autocommit by bot"
-git push
+git add report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml >> logs/git.log
+git commit report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml -m"autocommit by bot" >> logs/git.log
+git push >> logs/git.log
