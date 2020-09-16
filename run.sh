f68b9a4e36c29cb151e0cb12c11fd2931fe6ac79
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-09-16 03:10:26 +0200

    fix commit command

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 java -jar build/libs/software-updates-bot-0.0.1-SNAPSHOT.jar
 git add report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml >> logs/git.log
 git add report/status.md report/status.rss.xml report/status.atom.xml >> logs/git.log
-git commit report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml -m"autocommit by bot" >> logs/git.log
+git commit report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml report/status.md report/status.rss.xml report/status.atom.xml -m"autocommit by bot" >> logs/git.log
 git push >> logs/git.log
