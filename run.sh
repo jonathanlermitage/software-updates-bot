ade3facd4022f991f3e23a994e74280771e43e2a
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-12-19 03:25:09 +0100

    add datetime to git.log

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 java -jar build/libs/software-updates-bot-0.0.1-SNAPSHOT.jar
+date >> logs/git.log
 git add report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml >> logs/git.log
 git add report/status.md report/status.rss.xml report/status.atom.xml >> logs/git.log
 git commit report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml report/status.md report/status.rss.xml report/status.atom.xml -m"autocommit by bot [ci skip]" >> logs/git.log
