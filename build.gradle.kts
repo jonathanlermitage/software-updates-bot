f92e05b8893d0e349f025f6cb9902e2a5abee3b7
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2021-04-27 23:07:18 +0200

    upgrade libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val detektVersion = "1.16.0"
 
 plugins {
-    val kotlinVersion = "1.4.31"
+    val kotlinVersion = "1.5.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("kapt") version kotlinVersion
-    id("org.springframework.boot") version "2.4.4"
+    id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.github.ben-manes.versions") version "0.38.0"
     id("project-report") // https://docs.gradle.org/current/userguide/project_report_plugin.html
