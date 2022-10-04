2f2c6ad7005f3f25168a46dcccf3fcb79e1b3e66
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2022-10-04 15:52:07 -0400

    debug workflow

--- src/main/kotlin/biz/lermitage/sub/model/Report.kt
+++ src/main/kotlin/biz/lermitage/sub/model/Report.kt
@@ -2,4 +2,4 @@ package biz.lermitage.sub.model
 
 import java.util.Date
 
-data class Report(val updates: List<SoftwareUpdate>, val createdAt: Date = Date())
+data class Report(val updates: List<SoftwareUpdate>)
