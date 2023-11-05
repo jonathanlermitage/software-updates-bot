749710478e2993306c814696c9c748dd038914e6
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2023-11-05 20:00:33 +0100

    upgrade libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,19 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val detektVersion = "1.23.1" // IMPORTANT don't forget to update plugin version too
+val detektVersion = "1.23.3" // IMPORTANT don't forget to update plugin version too
 val prjJavaVersion = JavaVersion.VERSION_17
 
 plugins {
-    val kotlinVersion = "1.9.10"
+    val kotlinVersion = "1.9.20"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("kapt") version kotlinVersion
-    id("org.springframework.boot") version "3.1.4"
+    id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
-    id("com.github.ben-manes.versions") version "0.48.0"
+    id("com.github.ben-manes.versions") version "0.49.0"
     id("project-report") // https://docs.gradle.org/current/userguide/project_report_plugin.html
-    id("io.gitlab.arturbosch.detekt") version "1.23.1" // IMPORTANT set it to detektVersion's value
+    id("io.gitlab.arturbosch.detekt") version "1.23.3" // IMPORTANT set it to detektVersion's value
     id("biz.lermitage.oga") version "1.1.1"
 }
 
@@ -33,12 +33,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.jayway.jsonpath:json-path:2.8.0")
-    implementation("org.jsoup:jsoup:1.16.1") // https://jsoup.org/news/
-    implementation("commons-io:commons-io:2.13.0")
+    implementation("org.jsoup:jsoup:1.16.2") // https://jsoup.org/news/
+    implementation("commons-io:commons-io:2.15.0")
     implementation("org.apache.commons:commons-lang3:3.13.0")
     implementation("com.rometools:rome:2.1.0") // https://github.com/rometools/rome/releases
     implementation("org.springframework.boot:spring-boot-starter-aop")
-    implementation("org.springframework.retry:spring-retry:2.0.3")
+    implementation("org.springframework.retry:spring-retry:2.0.4")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
@@ -56,7 +56,7 @@ detekt {
 configurations.matching { it.name == "detekt" }.all {
     resolutionStrategy.eachDependency {
         if (requested.group == "org.jetbrains.kotlin") {
-            useVersion("1.9.0")
+            useVersion("1.9.10") // IMPORTANT update if failed with "detekt was compiled with Kotlin XX but is currently running with YY"
         }
     }
 }
