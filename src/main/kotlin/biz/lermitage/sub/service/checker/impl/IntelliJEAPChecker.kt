472bf268603a2d9729eadd7331b10bbfa3d6b4ae
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2024-08-27 20:44:46 +0200

    typo in doc

--- src/main/kotlin/biz/lermitage/sub/service/checker/impl/IntelliJEAPChecker.kt
+++ src/main/kotlin/biz/lermitage/sub/service/checker/impl/IntelliJEAPChecker.kt
@@ -4,7 +4,7 @@ import biz.lermitage.sub.service.checker.Checker
 import org.springframework.stereotype.Service
 
 /**
- * IntelliJ stable checker.
+ * IntelliJ EAP checker.
  */
 @Service
 class IntelliJEAPChecker : IntelliJChecker(false), Checker
