9eb0b384dbec5297128c7e3341bb314a7f921914
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2025-02-16 21:43:02 +0100

    more scrappers: properties, and one dedicated to JetBrains IDEs versions

--- run.sh
+++ run.sh
@@ -19,9 +19,9 @@ java -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Dfile.encoding=UTF-8 -jar b
 # git logging and push
 date >> logs/git.log
 
-git add report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml >> logs/git.log
+git add report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml report/report.properties report.uc.properties >> logs/git.log
 git add report/status.md report/status.rss.xml report/status.atom.xml >> logs/git.log
 
-git commit report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml report/status.md report/status.rss.xml report/status.atom.xml -m"autocommit by bot [ci skip]" >> logs/git.log
+git commit report/report.json report/report.md report/report-by-date.md report/report.rss.xml report/report.atom.xml  report/report.properties report.uc.properties report/status.md report/status.rss.xml report/status.atom.xml -m"autocommit by bot [ci skip]" >> logs/git.log
 
 git push >> logs/git.log
