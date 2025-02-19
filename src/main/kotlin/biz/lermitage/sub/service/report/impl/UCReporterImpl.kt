f4bba1aab57f2aed8d024d267fae58c8fad00e79
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2025-02-19 02:22:28 +0100

    Jb scrapper: add RR (Rust Rover)

--- src/main/kotlin/biz/lermitage/sub/service/report/impl/UCReporterImpl.kt
+++ src/main/kotlin/biz/lermitage/sub/service/report/impl/UCReporterImpl.kt
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.io.File
-import java.util.Date
 
 @Service
 class UCReporterImpl(private val conf: LocalAppConf) : Reporter {
@@ -38,7 +37,7 @@ class UCReporterImpl(private val conf: LocalAppConf) : Reporter {
     private fun write(reportFile: File) {
         val lines = ArrayList<String>()
 
-        val platformTypes = listOf("IU", "IC", "RD", "PC", "WS", "PS", "CL", "GO", "RM", "DG", "RS", "QA")
+        val platformTypes = listOf("IU", "IC", "RD", "PC", "WS", "PS", "CL", "GO", "RM", "DG", "RS", "QA", "RR")
 
         platformTypes.forEach { platformType ->
             run {
